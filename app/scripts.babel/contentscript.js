043535ff7af78b5cd9a720c24fc9a835f389e9c4
塔洛 <547098912@qq.com>
2017-11-17 18:48:45 +0800

    add main logic

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,3 +1,34 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Content script');
+console.log('js debug helper running');
+
+function injectScript(scriptString) {
+  let actualCode = '(function(){' + scriptString + '})();';
+  let script = document.createElement('script');
+  script.textContent = actualCode;
+  (document.head || document.documentElement).appendChild(script);
+  // script.parentNode.removeChild(script);
+}
+
+function getScript(path) {
+  let url = chrome.runtime.getURL(path);
+  fetch(url).then(res => {
+    if (res.ok) {
+      return res.text();
+    } else {
+      console.log(`fetch ${path} failed`);
+    }
+  }).then(scriptSource => {
+    // console.log(scriptSource);
+    injectScript(scriptSource);
+  })
+}
+
+let helperFileList = [
+  'diff_window_obj.js'
+];
+
+helperFileList.forEach(file => {
+  let path = `scripts/helpers/${file}`;
+  getScript(path);
+});
