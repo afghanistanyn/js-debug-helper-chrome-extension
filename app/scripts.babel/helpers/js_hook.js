54c36e248d3e64020739a5f3d869edbe2b903876
塔洛 <547098912@qq.com>
2017-12-15 10:24:20 +0800

    init js_hook & add hook_document_domain

---
+++ app/scripts.babel/helpers/js_hook.js
@@ -0,0 +1,33 @@
+window.__hookjsLog = function (msg, color = 'red') {
+  console.log(`%cHookJS: ${msg}`, `color:${color}`);
+};
+window.__hookjsTrace = function (trace = false) {
+  if (trace) {
+    console.trace();
+  }
+};
+window.__hookjs = {
+  hook_document_domain: function (trace) {
+    let oriSetter = document.__lookupSetter__('domain');
+    let oriGetter = document.__lookupGetter__('domain');
+    Object.defineProperty(document, 'domain', {
+      get: function () {
+        __hookjsLog('Get document.domain', '#000');
+        __hookjsTrace(trace);
+        return oriGetter.apply(this, arguments)
+      },
+      set: function () {
+        __hookjsLog(`SET document.domain, before set: ${document.domain}, after set: ${arguments[0]}`); // 这里也读取了 document.domain，所以会触发一次 get hook
+        __hookjsTrace(trace);
+        return oriSetter.apply(this, arguments)
+      }
+    })
+  }
+};
+
+(function main() {
+  Object.keys(window.__hookjs).forEach(key => {
+    window.__hookjs[key](true);
+  });
+  console.log('js hook finish...');
+})();
